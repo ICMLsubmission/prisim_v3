66a8458fb6d28ab8ff02b19eae6f90e18d6a8f83
Subhrajit Samanta <46675082+ICMLsubmission@users.noreply.github.com>
2026-03-06 15:16:10 +0530

    Update app.py

--- privacy/app.py
+++ privacy/app.py
@@ -1,30 +1,20 @@
 import os
-import yaml
 import traceback
-from .src.config import Config
-from .design.file_upload import fileUpload
-from .design.data_processing import dataProcessing
-from .design.reporting import dataProfiling
-from .design.evaulate import evaluate
-from .design.training_generation_fe import trainingGeneration
-from .design.privacy_generation_fe import Privacy
-from .design.loading_generating import pretrainedModelGeneration
 from IPython.display import HTML, display
+
+# Lightweight imports only - always needed
+from .design.file_upload import fileUpload
 
 
 def read_configurations_yaml(file_path):
+    import yaml
     with open(file_path, "r") as f:
         return yaml.safe_load(f)
 
 
-class dataPrivacy(Config):
+class dataPrivacy:
     def __init__(self):
         os.chdir("../")
-        # self.configurations = read_configurations_yaml(
-        #     "privacy/configurations/privacy_configurations.yaml"
-        # )["PRIVACY"]
-        # if self.configurations["ENVIRONMENT"] == "test":
-        #     super().__init__(jsonpath="privacy/configurations/params.json")
         self.loadingPretrainedModel = False
         self.dataEvaluate = None
         self.EDA = False
@@ -33,21 +23,17 @@ class dataPrivacy(Config):
         self.datareader = fileUpload(psuedoMode)
 
     def dataPreProcessing(self):
+        from .design.data_processing import dataProcessing
         try:
             self.filename_prefix = self.datareader.filename_prefix
             self.data = self.datareader.dataFrame
             self.preProcesser = dataProcessing(self.data.copy())
             self.preProcesser.display_data_processing()
-        except:
-            if self.configurations["DEBUG"]:
-                print(traceback.format_exc())
-            display(
-                HTML(
-                    '<hr><h4 style="color:red">Oho something went wrong, Don\'t Worry!! <br>Please run above blockcells</h4><hr>'
-                )
-            )
+        except Exception:
+            display(HTML('<hr><h4 style="color:red">Something went wrong. Please re-run above cells.</h4><hr>'))
 
     def dataReporting(self):
+        from .design.reporting import dataProfiling
         try:
             self.processedData = self.preProcesser.processedData
             self.encodedDf = self.preProcesser.encodedDf
@@ -65,14 +51,34 @@ class dataPrivacy(Config):
                 addressColumns=self.addressColumns,
                 discreteLabelEncoders=self.discreteLabelEncoders,
             )
-        except:
-            if self.configurations["DEBUG"]:
-                print(traceback.format_exc())
-            display(
-                HTML(
-                    '<hr><h4 style="color:red">Oho something went wrong, Don\'t Worry!! <br>Please run above blockcells</h4><hr>'
-                )
+        except Exception:
+            display(HTML('<hr><h4 style="color:red">Something went wrong. Please re-run above cells.</h4><hr>'))
+
+    def training(self):
+        from .design.training_generation_fe import trainingGeneration
+        from .src.config import Config
+        try:
+            if not self.EDA:
+                self.mapObjectValuesToVaribales(self.preProcesser)
+            self.model = trainingGeneration(
+                self.encodedDf,
+                self.discreteColumns,
+                self.nameColumns,
+                self.addressColumns,
+                originalData=self.processedData,
+                discreteLabelEncoders=self.discreteLabelEncoders,
+                dataEvaluateObj=self.dataEvaluate,
+                filename_prefix=self.filename_prefix,
             )
+            self.model.train()
+        except Exception:
+            display(HTML('<hr><h4 style="color:red">Something went wrong. Please re-run above cells.</h4><hr>'))
+
+    def loadModelGenerateData(self):
+        from .design.loading_generating import pretrainedModelGeneration
+        self.modelObj = pretrainedModelGeneration()
+        self.modelObj.loadingModel()
+        self.loadingPretrainedModel = True
 
     def mapObjectValuesToVaribales(self, object):
         if self.loadingPretrainedModel:
@@ -92,35 +98,6 @@ class dataPrivacy(Config):
             self.nameColumns = object.nameColumns
             self.addressColumns = object.addressColumns
 
-    def training(self):
-        try:
-            if not self.EDA:
-                self.mapObjectValuesToVaribales(self.preProcesser)
-            self.model = trainingGeneration(
-                self.encodedDf,
-                self.discreteColumns,
-                self.nameColumns,
-                self.addressColumns,
-                originalData=self.processedData,
-                discreteLabelEncoders=self.discreteLabelEncoders,
-                dataEvaluateObj=self.dataEvaluate,
-                filename_prefix=self.filename_prefix,
-            )
-            self.model.train()
-        except:
-            if self.configurations["DEBUG"]:
-                print(traceback.format_exc())
-            display(
-                HTML(
-                    '<hr><h4 style="color:red">Oho something went wrong, Don\'t Worry!! <br>Please run above blockcells</h4><hr>'
-                )
-            )
-
-    def loadModelGenerateData(self):
-        self.modelObj = pretrainedModelGeneration()
-        self.modelObj.loadingModel()
-        self.loadingPretrainedModel = True
-
     def generateSyntheticData(self):
         try:
             self.filename_prefix = self.modelObj.filename_prefix
@@ -131,16 +108,11 @@ class dataPrivacy(Config):
             self.nameColumns = self.modelObj.nameColumns
             self.addressColumns = self.modelObj.addressColumns
             self.modelObj.pretrainedModelObj.samplesSelection()
-        except:
-            if self.configurations["DEBUG"]:
-                print(traceback.format_exc())
-            display(
-                HTML(
-                    '<hr><h4 style="color:red">Oho something went wrong, Don\'t Worry!! <br>Please run above blockcells</h4><hr>'
-                )
-            )
+        except Exception:
+            display(HTML('<hr><h4 style="color:red">Something went wrong. Please re-run above cells.</h4><hr>'))
 
     def privacyGenerationReporting(self):
+        from .design.privacy_generation_fe import Privacy
         try:
             self.n_samples = self.modelObj.pretrainedModelObj.samplesValue
             self.syntheticData = self.modelObj.pretrainedModelObj.syntheticData
@@ -156,11 +128,5 @@ class dataPrivacy(Config):
             )
             self.privacyModule.privacyBuild()
             self.privacyModule.validationGenerationExploration(self.n_samples)
-        except:
-            if self.configurations["DEBUG"]:
-                print(traceback.format_exc())
-            display(
-                HTML(
-                    '<hr><h4 style="color:red">Oho something went wrong, Don\'t Worry!! <br>Please run above blockcells</h4><hr>'
-                )
-            )
+        except Exception:
+            display(HTML('<hr><h4 style="color:red">Something went wrong. Please re-run above cells.</h4><hr>'))
